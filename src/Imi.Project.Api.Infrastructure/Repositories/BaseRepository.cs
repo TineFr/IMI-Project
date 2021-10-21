d183f8d98a2cfc87f7a716dda514bb74944af086
Tine Franchois <tine.franchois@student.howest.be>
2021-10-21 17:01:36 +0200

    Add IBaseRepository implementation

---
+++ src/Imi.Project.Api.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using Imi.Project.Api.Core.Entities;
+using Imi.Project.Api.Core.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly AppDbContext _dbContext;
+
+        public BaseRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public  IQueryable<T> GetAll()
+        {
+           return  _dbContext.Set<T>().AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var entity = await  _dbContext.Set<T>().FirstOrDefaultAsync(e => e.Id.Equals(id));
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> ListAllAsync()
+        {
+          return await  _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
