6cbf9326cdea87a2b1d97c38401bbc296fb58268
Tine Franchois <tine.franchois@student.howest.be>
2021-10-21 19:43:35 +0200

    Add swagger

--- src/Imi.Project.Api/Startup.cs
+++ src/Imi.Project.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,10 @@ namespace Imi.Project.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "IMI API", Version = "v1" });
+            });
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer
             (Configuration.GetConnectionString("MyAviaryDataBase")));
         }
@@ -35,6 +40,12 @@ namespace Imi.Project.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IMI API v1");
+
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
