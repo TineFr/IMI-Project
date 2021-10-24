c23370583e50402b1518bfb865393f0a6b81209b
Tine Franchois <tine.franchois@student.howest.be>
2021-10-24 18:37:52 +0200

    Add SpeciesController

---
+++ src/Imi.Project.Api/Controllers/SpeciesController.cs
@@ -0,0 +1,37 @@
+﻿using Imi.Project.Api.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeciesController : ControllerBase
+    {
+        protected readonly ISpeciesService _speciesService;
+
+        public SpeciesController(ISpeciesService speciesService)
+        {
+            _speciesService = speciesService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var species = await _speciesService.ListAllSpeciessAsync();
+            return Ok(species);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var species = await _speciesService.GetSpeciesByIdAsync(id);
+            return Ok(species);
+        }
+    }
+}
--- src/Imi.Project.Api/Startup.cs
+++ src/Imi.Project.Api/Startup.cs
@@ -49,6 +49,7 @@ namespace Imi.Project.Api
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ICageService, CageService>();
             services.AddScoped<IBirdService, BirdService>();
+            services.AddScoped<ISpeciesService, SpeciesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
