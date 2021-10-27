e8d37414a458688c6014f577d37232487520b0ed
Tine Franchois <tine.franchois@student.howest.be>
2021-10-27 19:32:44 +0200

    Add MockBirdService

--- src/Imi.Project.Blazor/Models/Bird.cs
+++ src/Imi.Project.Blazor/Models/Bird.cs
@@ -7,9 +7,15 @@ namespace Imi.Project.Blazor.Models
 {
     public class Bird : BaseEntity
     {
-        public string Image { get; set; }
+        public DateTime HatchDate { get; set; }
         public string Gender { get; set; }
-        public Species Species { get; set; }
-        public Cage Cage { get; set; }
+        public string Species { get; set; }
+        public string ScientificName { get; set; }
+        public string Food { get; set; }
+        public string Cage { get; set; }
+        public string Image { get; set; }
+        public string GenderImage => $"{Gender}.png";
+        public string Date => HatchDate.ToString("dd/MM/yyyy");
     }
+
 }
---
+++ src/Imi.Project.Blazor/Services/Interfaces/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Services.Interfaces
+{
+    public interface IBaseService<T>
+    {
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task<bool> UpdateAsync(T item);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
