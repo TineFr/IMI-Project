4a3a6c56708846d0533ec0ed78af98ed6bb06209
Tine Franchois <tine.franchois@student.howest.be>
2021-11-03 16:57:46 +0100

    Add Pagination classes

---
+++ src/Imi.Project.Api.Core/Entities/Pagination/PaginationParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Entities.Pagination
+{
+    public class PaginationParameters
+    {
+        public int MaxItemsPerPage { get; set; } = 20;
+
+        private int itemsPerPage;
+
+        public int ItemsPerPage
+        {
+            get { return itemsPerPage; }
+            set { itemsPerPage = itemsPerPage > MaxItemsPerPage ? MaxItemsPerPage : value;}
+        }
+        public int CurrentPage { get; set; } = 1;
+
+
+    }
+}
---
+++ src/Imi.Project.Api.Core/Helper/Pagination.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imi.Project.Api.Core.Helper
+{
+    public class Pagination
+    {
+    }
+}
