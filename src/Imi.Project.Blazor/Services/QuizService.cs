5731183d1c50f5b61992e96c5987e31a490884cf
Tine Franchois <tine.franchois@student.howest.be>
2021-11-06 18:37:49 +0100

    Update seeding data

--- src/Imi.Project.Blazor/Services/QuizService.cs
+++ src/Imi.Project.Blazor/Services/QuizService.cs
@@ -17,63 +17,65 @@ namespace Imi.Project.Blazor.Services
                         Image = "images/quiz/americanflag.png",
                         PossibleAnswers = new List<QuizAnswer>()
                         {
-
                             new QuizAnswer("Bald Eagle", true),
                             new QuizAnswer("Golden Eagle", false),
                             new QuizAnswer("Raven", false),
                             new QuizAnswer("Mockingbird", false),
-
-                        },
-       
-                        
+                        }                   
                     },
 
-                    //new QuizElement
-                    //{
-                    //    Question = "What is the world’s smallest living bird?",
-                    //    Image = "images/quiz/beehummingbird.jpg",
-                    //    PossibleAnswers = new List<string>()
-                    //    {
-                    //        "Bee Hummingbird", "Diamond Firetail", "Grey Fantail", "Spotted Pardalote"
-                    //    },
-                    //    CorrectAnswer="Bee Hummingbird"
+                    new QuizElement
+                    {
+                        Question = "What is the world’s smallest living bird?",
+                        Image = "images/quiz/beehummingbird.jpg",
+                        PossibleAnswers = new List<QuizAnswer>()
+                        {
+                            new QuizAnswer("Bee Hummingbird", true),
+                            new QuizAnswer("Diamond Firetail", false),
+                            new QuizAnswer("Grey Fantail", false),
+                            new QuizAnswer("Spotted Pardalote", false),
+                        }
+                    },
 
-                    //},
+                    new QuizElement
+                    {
+                        Question = "Which bird has the fastest maximum airspeed and is classified as the fastest animal in the world?",
+                        Image = "images/quiz/fastestbird.jpg",
+                        PossibleAnswers = new List<QuizAnswer>()
+                        {    
+                            new QuizAnswer("Saker Falcon", false),
+                            new QuizAnswer("Golden Eagle", false),
+                            new QuizAnswer("Peregrine Falcon", true),
+                            new QuizAnswer("Grey-headed Albatross", false),
+                        }
+                    },
 
-                    //new QuizElement
-                    //{
-                    //    Question = "Which bird has the fastest maximum airspeed and is classified as the fastest animal in the world?",
-                    //    Image = "images/quiz/fastestbird.jpg",
-                    //    PossibleAnswers = new List<string>()
-                    //    {
-                    //        "Peregrine Falcon", "Saker falcon", "Golden Eagle", "Grey-headed Albatross"
-                    //    },
-                    //    CorrectAnswer="Peregrine Falcon"
+                    new QuizElement
+                    {
+                        Question = "What is the collective name for a group of crows?",
+                        Image = "images/quiz/crows.jfif",
+                        PossibleAnswers = new List<QuizAnswer>()
+                        {
+                            new QuizAnswer("A Wreck", false),
+                            new QuizAnswer("A Murder", true),
+                            new QuizAnswer("A Flock", false),
+                            new QuizAnswer("A Muster", false),
+                        }
+                    },
 
-                    //},
 
-                    //new QuizElement
-                    //{
-                    //    Question = "What is the collective name for a group of crows?",
-                    //    Image = "images/quiz/crows.jfif",
-                    //    PossibleAnswers = new List<string>()
-                    //    {
-                    //        "A Wreck", "A Murder", "A Flock", "A Muster"
-                    //    },
-                    //    CorrectAnswer="A Murder"
-
-                    //},
-                    //new QuizElement
-                    //{
-                    //    Question = "Which birds did coal miners traditionally bring into the mines with them to detect dangerous levels of carbon monoxide?",
-                    //    Image = "images/quiz/mine.jfif",
-                    //    PossibleAnswers = new List<string>()
-                    //    {
-                    //        "Robins", "Crows", "Canaries", "Blackbirds"
-                    //    },
-                    //    CorrectAnswer="Canaries"
-
-                    //},
+                    new QuizElement
+                    {
+                        Question = "Which birds did coal miners traditionally bring into the mines with them to detect dangerous levels of carbon monoxide?",
+                        Image = "images/quiz/mine.jfif",
+                        PossibleAnswers = new List<QuizAnswer>()
+                        {
+                            new QuizAnswer("Robins", false),
+                            new QuizAnswer("Blackbirds", false),
+                            new QuizAnswer("Canaries", true),
+                            new QuizAnswer("Ravens", false),
+                        }
+                    },
 
 
                     //new QuizElement
