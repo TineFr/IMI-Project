7b36d67316f6e697302c1331e9d03f4259fac8b6
Tine Franchois <tine.franchois@student.howest.be>
2022-01-13 10:45:22 +0100

    Import axios

--- src/Imi.Project.Vue/wwwroot/js/site.js
+++ src/Imi.Project.Vue/wwwroot/js/site.js
@@ -2,17 +2,47 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let app = new Vue({
-    el: '#app',
+
+var isInVisible  = true;
+
+const globalStore = new Vue({
     data: {
-        message: `Vue in the works!`
+        isInVisible: true,
     }
 })
 
-let layout = new Vue({
+
+var login = new Vue({
+    el: '#divLogIn',
+    data: {
+        isInVisible,
+    },
+    methods: {
+        ToggleNavBarVisibility: function () {
+            var self = this;
+            self.isInVisible = true;
+        },
+    created: function () {
+        this.appName = "test worked";
+    }
+    }});
+
+var layout = new Vue({
     el: 'header',
     data: {
-        isInVisible: false,
+        isInVisible
     }
 })
 
+
+var selectionBoxes = new Vue({
+    el: ".paginationDiv",
+    data: {
+        ShowSelectionBoxes: false,
+    },
+    methods: {
+        ToggleShowSelectionBoxes: function () {
+            this.ShowSelectionBoxes = !this.ShowSelectionBoxes;
+        },
+    }
+});
