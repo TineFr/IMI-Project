f3c6a63934b19c494731b1f5f712b702d1d588a1
Tine Franchois <tine.franchois@student.howest.be>
2022-01-23 00:05:57 +0100

    Implement api auth service

--- src/Imi.Project.Blazor/Models/Api/Authentication/LoginRequestModel.cs
+++ src/Imi.Project.Blazor/Models/Api/Authentication/LoginRequestModel.cs
@@ -7,7 +7,12 @@ namespace Imi.Project.Blazor.Models.Api
 { 
     public class LoginRequestModel
     {
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Must be a valid email adress")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
